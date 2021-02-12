5b31c1bcd27cd9763fe5bf4aa8766b6da886649a
bran <bran@watcha.com>
2021-02-12 21:37:24 +0900

    UPdate: handle connection 1
    - request stream buffer ㅇㅔ 담아 utf8 로 읽기

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("{}", String::from_utf8_lossy(&buffer));
+}
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("ttl: {}", stream.ttl().unwrap());
-        println!("연결됨");
+    for stream_result in listener.incoming() {
+        let str = stream_result.unwrap();
+        handle_connection(str);
     }
 }
